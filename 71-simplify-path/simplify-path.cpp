3e4e15e67494691ab39ac8f3cfbe8c2bcd751979
Rohit Kumar <rohitkumar178.24@kgpian.iitkgp.ac.in>
2026-08-05 11:05:21 +0530

    Time: 3 ms (63.40%) | Memory: 11.1 MB (65.46%) - LeetSync

---
+++ 71-simplify-path/simplify-path.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        vector<string> st;
+        stringstream ss(path);
+        string token;
+        while (getline(ss, token, '/')) {
+            if (token == "" || token == ".") {
+                continue;
+            }
+            if (token == "..") {
+                if (!st.empty()) {
+                    st.pop_back();
+                }
+            } else {
+                st.push_back(token);
+            }
+        }
+
+        string ans = "";
+        for (auto it : st) {
+            ans += "/" + it;
+        }
+        return ans.empty() ? "/" : ans;
+    }
+};
